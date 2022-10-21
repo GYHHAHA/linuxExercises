9cdb365a6febd46ad47bfcadd299c7b850147787
Xinyan Wang <xwang2587@wisc.edu>
2022-10-20 22:02:30 -0500

    Updated rm_n.sh.

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+dir=$1
+file_size=$2
+if [ $# -ne 2 ]
+then
+    echo "usage: rm_n.sh <dir> n"
+    exit
+fi
+for file in `ls $dir`
+do
+  if [ -f "$file" ];then
+   echo $file
+   size=$(wc -c < $file)
+   if [ $size -gt $file_size ];then
+     rm -rf $file
+     echo "$file size : $size is removed."
+   fi
+  fi
+done
